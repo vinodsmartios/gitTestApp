e34c9cd3ebd026aec62d0d247183999a7b981017
iosVinodKumar <vinodkumar1236.ios@gmail.om>
2020-11-30 19:32:55 +0530

    Initial Commit

---
+++ gitTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitTestApp
+//
+//  Created by vinodkumar on 30/11/20.
+//  Copyright © 2020 vinodkumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
